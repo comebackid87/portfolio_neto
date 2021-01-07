d21e11cf44d6c40617d8e7527f740d61b03dfa33
comebackid87 <vieiromeister05@gmail.com>
2021-01-07 15:10:28 +0000

    initial commit

---
+++ pages/portfoliosNotUsed/[id].js
@@ -0,0 +1,34 @@
+import React from 'react'
+import BaseLayout from '../../components/layouts/BaseLayout'
+import axios from 'axios'
+import {withRouter} from 'next/router'
+
+class Portfolio extends React.Component {
+
+    static async getInitialProps({query}) {
+        let post = {}
+
+        try {
+            const res = await axios.get(`http://jsonplaceholder.typicode.com/posts/${query.id}`)
+            post = res.data
+        } catch(error) {
+            console.log(error)
+        }
+
+        return {portfolio: post}
+    }
+
+    render() {
+        const {portfolio} = this.props
+        return (
+            <BaseLayout>
+                <h1>Portfolio</h1>
+                <h1>{portfolio.title}</h1>
+                <p>Body: {portfolio.body}</p>
+                <p>ID: {portfolio.id}</p>
+            </BaseLayout>
+        )
+    }
+}
+
+export default withRouter(Portfolio)
