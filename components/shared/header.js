ca25d1d9c56f9f388c44da3521e16263f8d3b240
comebackid87 <vieiromeister05@gmail.com>
2021-02-17 22:38:03 +0000

    added admin navigation, blog pages and links

--- components/shared/header.js
+++ components/shared/header.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
+import { isAuthorized } from '@/utils/auth0'
 import {
     Collapse,
     Navbar,
     NavbarToggler,
     Nav,
-    NavItem
+    NavItem,
+    Dropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
 } from 'reactstrap';
 import Link from 'next/link';
 
 const BsNavLink = props => {
-  const {title, href} = props
+  const {title, href, className=''} = props
   return (
     <Link href={href}>
-      <a className="nav-link port-navbar-link">{title}</a>
+      <a className={`nav-link port-navbar-link ${className}`}>{title}</a>
     </Link>
   )
 }
@@ -27,6 +32,39 @@ const LoginLink = () =>
 
 const LogoutLink = () => 
   <a className="nav-link port-navbar-link" href="/api/v1/logout">Logout</a>
+
+const AdminMenu = () => {
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <Dropdown
+      className="port-navbar-link port-dropdown-menu"
+      nav
+      isOpen={isOpen}
+      toggle={() => setIsOpen(!isOpen)}
+    >
+      <DropdownToggle
+        className="port-dropdown-toggle"
+        nav
+        caret
+      >
+        Admin
+      </DropdownToggle>
+      <DropdownMenu right>
+        <DropdownItem>
+          <BsNavLink className="port-dropdown-item" href="/portfolios/new" title="Create Portfolio" />
+        </DropdownItem>
+        <DropdownItem>
+          <BsNavLink className="port-dropdown-item" href="/blogs/editor" title="Blog Editor" />
+        </DropdownItem>
+        <DropdownItem>
+          <BsNavLink className="port-dropdown-item" href="/blogs/dashboard" title="Dashboard" />
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  )
+}
 
 const Header = ({user, loading, className}) => {
 
@@ -62,9 +100,12 @@ const Header = ({user, loading, className}) => {
                 <>
                   {
                     user && 
-                    <NavItem className="port-navbar-item">
-                      <LogoutLink />
-                    </NavItem>
+                    <>
+                      { isAuthorized(user, 'admin') && <AdminMenu /> }
+                      <NavItem className="port-navbar-item">
+                        <LogoutLink />
+                      </NavItem>
+                    </>
                   }
                   {
                     !user &&
